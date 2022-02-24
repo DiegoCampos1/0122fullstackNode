91d1bbd7671aa99bd9cbe1b554761653fd98cd26
Diego Campos <diegodiko05@gmail.com>
2022-02-24 19:16:55 -0300

    exercise 01 resolve

--- exercisesNode/01.js
+++ exercisesNode/01.js
@@ -1,35 +1,48 @@
 // Você recebeu o seguinte array, faça as funções solicitadas abaixo:
 const estados = [
-  { 'AC': 'Acre' },
-  { 'AL': 'Alagoas' },
-  { 'AP': 'Amapá' },
-  { 'AM': 'Amazonas' },
-  { 'BA': 'Bahia' },
-  { 'CE': 'Ceará' },
-  { 'DF': 'Distrito Federal' },
-  { 'ES': 'Espírito Santo' },
-  { 'GO': 'Goías' },
-  { 'MA': 'Maranhão' },
-  { 'MT': 'Mato Grosso' },
-  { 'MS': 'Mato Grosso do Sul' },
-  { 'MG': 'Minas Gerais' },
-  { 'PA': 'Pará' },
-  { 'PB': 'Paraíba' },
-  { 'PR': 'Paraná' },
-  { 'PE': 'Pernambuco' },
-  { 'PI': 'Piauí' },
-  { 'RJ': 'Rio de Janeiro' },
-  { 'RN': 'Rio Grande do Norte' },
-  { 'RS': 'Rio Grande do Sul' },
-  { 'RO': 'Rondônia' },
-  { 'RR': 'Roraíma' },
-  { 'SC': 'Santa Catarina' },
-  { 'SP': 'São Paulo' },
-  { 'SE': 'Sergipe' },
-  { 'TO': 'Tocantins' },
-],
-
+  { AC: 'Acre' },
+  { AL: 'Alagoas' },
+  { AP: 'Amapá' },
+  { AM: 'Amazonas' },
+  { BA: 'Bahia' },
+  { CE: 'Ceará' },
+  { DF: 'Distrito Federal' },
+  { ES: 'Espírito Santo' },
+  { GO: 'Goías' },
+  { MA: 'Maranhão' },
+  { MT: 'Mato Grosso' },
+  { MS: 'Mato Grosso do Sul' },
+  { MG: 'Minas Gerais' },
+  { PA: 'Pará' },
+  { PB: 'Paraíba' },
+  { PR: 'Paraná' },
+  { PE: 'Pernambuco' },
+  { PI: 'Piauí' },
+  { RJ: 'Rio de Janeiro' },
+  { RN: 'Rio Grande do Norte' },
+  { RS: 'Rio Grande do Sul' },
+  { RO: 'Rondônia' },
+  { RR: 'Roraíma' },
+  { SC: 'Santa Catarina' },
+  { SP: 'São Paulo' },
+  { SE: 'Sergipe' },
+  { TO: 'Tocantins' },
+];
 
 // 01- Faça uma função que espera receber um parámetro, uma letra, e retorna um array com a(s) sigla(s) do(s) estado(s) que começam com aquela letra.
 // por exemplo se sua função receber como parametro a letra "T", ele deve retornar apenas ['TO']
 // Se sua função receber como paramentro a letra "R", deve retornar ['RJ', 'RN', 'RS', 'RO', 'RR']
+
+// console.log(Object.keys({ TO: 'Tocantins' }));
+
+const filtraEstado = (letra) => {
+  const auxiliar = estados.filter(
+    (estado) => Object.keys(estado).toString()[0] === letra
+  );
+
+  const response = auxiliar.map((estado) => Object.keys(estado).toString());
+
+  return response;
+};
+
+console.log(filtraEstado('R'));
